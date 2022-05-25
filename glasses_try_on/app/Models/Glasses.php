317b877b7f458a30fccba56ec4282654e67a9b14
Sarmad Sohail <sarmad-sohail@hotmail.com>
2022-05-25 23:51:05 +0500

    dynamic img

--- glasses_try_on/app/Models/Glasses.php
+++ glasses_try_on/app/Models/Glasses.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Glasses extends Model
 {
     use HasFactory;
+
+    public function img():Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => env('APP_URL').'/storage/'.$value,
+        );
+    }
+
 }
